c8101ba77a113593e4f340366e83466bebfd6d32
malek <malekfrikhi@gmail.com>
2018-08-07 13:15:39 +0200

    gg

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+          /* int  c = a + b;
+           int d = a - b;
+           int e = a * b;
+           int f = a / b;*/
+           /* Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);*/
+            string op =Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(a + b);  
+                    break;
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+                case "/":
+                    Console.WriteLine(a / b);
+                    break;
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+                    
+           
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
